e87ba68fb7fcfe13cff679d04710042e1a677516
Ali AlSubhi <alialsubhi1998@gmail.com>
2023-04-11 13:02:51 +0400

    create the table in data base with

--- src/main/java/org/example/JDBC.java
+++ src/main/java/org/example/JDBC.java
@@ -48,5 +48,58 @@ public class JDBC {
         return JDBC.Login;
     }
 
+    public void initializeDatabase() {
+        System.out.println("Initialize Database");
+        Scanner scanner = new Scanner(System.in);
 
+        String url = "jdbc:sqlserver://" + "localhost:1433;" +
+                "encrypt=true;" +
+                "trustServerCertificate=true";
+        Connection con = null;
+
+        try {
+            Driver driver = (Driver) Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver").newInstance();
+            DriverManager.registerDriver(driver);
+
+            con = DriverManager.getConnection(url, userName, password);
+            Statement st = con.createStatement();
+
+            // Check if the database exists
+            String sql1 = "SELECT * FROM sys.databases WHERE name='" + databaseName + "'";
+            ResultSet rs = st.executeQuery(sql1);
+
+            if (rs.next()) {
+                // Update url with the existing database name
+                url += ";databaseName=" + databaseName;
+                con = DriverManager.getConnection(url, userName, password);
+                Statement st2 = con.createStatement();
+
+                // Check if the universities table exists
+                String sql2 = "SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'ArticlesTable'";
+                rs = st2.executeQuery(sql2);
+
+                if (!rs.next()) {
+                    // Create table if it doesn't exist
+                    String sql3 = "CREATE TABLE ArticlesTable (\n" +
+                            "    ArticleID INT PRIMARY KEY IDENTITY,\n" +
+                            "    Title VARCHAR(250) ,\n" +
+                            "    Author VARCHAR(250) ,\n" +
+                            "    PublicationDate DATE ,\n" +
+                            "    Category VARCHAR(250) ,\n" +
+                            "    Content TEXT\n" +
+                            ");";
+                    st2.executeUpdate(sql3);
+                    System.out.println("ArticlesTable Table created successfully!");
+                } else {
+                    System.out.println("ArticlesTable Table already exists!");
+                }
+            } else {
+                System.out.println("Database does not exist.");
+            }
+
+            con.close();
+        } catch (Exception ex) {
+            System.err.println(ex);
+        }
+    }
 }
